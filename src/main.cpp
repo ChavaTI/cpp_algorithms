23683472d0f7204467d7443f1f26a09877051821
codeDude <chavatloz@gmail.com>
2022-09-06 23:20:09 -0500

    Add version system

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "iostream"
 #include "sum.h"
+#include "config.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+  
+  std::cout << argv[0] << " Version " << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR << '\n';
   std::cout << "hello" << "\n";
   std::cout << sum(1,1) << "\n";
   return EXIT_SUCCESS;
