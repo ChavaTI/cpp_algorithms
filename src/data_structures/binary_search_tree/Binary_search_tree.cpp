8cd1217b0314a88657ffd1e0c3554321023d9895
codeDude <chavatloz@gmail.com>
2023-02-07 21:06:38 -0600

    add getter and setter to root node in the binary-search-tree

--- src/data_structures/binary_search_tree/Binary_search_tree.cpp
+++ src/data_structures/binary_search_tree/Binary_search_tree.cpp
@@ -7,13 +7,21 @@ Binary_search_tree::Binary_search_tree(int rootValue) {
   root = new Node(rootValue);
 }
 
+void Binary_search_tree::setRoot(Node *newRoot) {
+  this->root = newRoot;
+}
+
+Node* Binary_search_tree::getRoot() {
+  return this->root;
+}
+
 void Binary_search_tree::insertNode(int newValue) {
   Node *newNode = new Node(newValue);
 
   if (this->root == NULL) {
     this->root = newNode;
   } else {
-    Node *currentNode = this->root;
+    Node *currentNode = this->getRoot();
     while (true) {
       if (newValue <= currentNode->getData()) {
         if (currentNode->getLeft() == NULL) {
