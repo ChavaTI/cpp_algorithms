0d92eadf28fdb45387c536afdc42168bee9cf3aa
codeDude <chavatloz@gmail.com>
2023-07-13 22:23:28 -0600

    :rotating_light: Linter

--- src/data_structures/binary_search_tree/Binary_search_tree.h
+++ src/data_structures/binary_search_tree/Binary_search_tree.h
@@ -1,21 +1,21 @@
 #ifndef BINARY_SEARCH_TREE
 #define BINARY_SEARCH_TREE
 
-#include <iostream>
 #include "node/Node.h"
+#include <iostream>
 
 class Binary_search_tree {
 private:
   Node *root;
   int calculateHeight(Node *node);
-  void insertNode(Node* node ,int value);
+  void insertNode(Node *node, int value);
 
 public:
   Binary_search_tree();
   Binary_search_tree(int rootValue);
 
-  void setRoot(Node* node);
-  Node* getRoot();
+  void setRoot(Node *node);
+  Node *getRoot();
   void insert(int value);
   void insertNodesFromArray(int *array, int length);
   void removeNode();
@@ -24,7 +24,7 @@ public:
   void inOrderTraverse();
   void postOrderTraverse();
   int getHeight();
-  void printTree(Node* node, const std::string& prefix, bool isLeft);
+  void printTree(Node *node, const std::string &prefix, bool isLeft);
   void print();
   Node *getMaximumNode();
   Node *getMinimumNode();
