0876f11633a312bdc40aa9498edc7f301c668e7a
codeDude <chavatloz@gmail.com>
2023-08-16 23:40:54 -0600

    feature/remove-node - Add test to getMinimumValue method

--- tests/data_structures/binary_search_tree/binary_search_tree.cpp
+++ tests/data_structures/binary_search_tree/binary_search_tree.cpp
@@ -98,6 +98,19 @@ TEST(bst, search_node) {
 
 }
 
+TEST(bst, get_minimum_value) {
+  int array[] = {50, 30, 70, 20, 40, 60, 80};
+  int length = sizeof(array) / sizeof(array[0]);
+  int expectedMinimumValue = 20;
+
+  Binary_search_tree *bst = new Binary_search_tree();
+
+  bst->insertNodesFromArray(array, length);
+  int minimumValue = bst->getMinimumValue();
+
+  EXPECT_EQ(minimumValue, expectedMinimumValue);
+}
+
 TEST(bst, remove_node) {
   int array[] = {50, 30, 70, 20, 40, 60, 80};
   int length = sizeof(array) / sizeof(array[0]);
